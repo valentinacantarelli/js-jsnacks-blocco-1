f642cb025a8a66b838045b86f77edc838c0abba1
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-25 17:08:43 +0200

    aggiornato esercizio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
 var listaNumeriDispari = [];
+var pariDispari;
 
 for(var i=0; i < 6; i++){
     numeroUtente = parseInt(prompt("Inserisci un numero:"));
-}
-var pariDispari;
-if( numeroUtente % 2 == 0 ) {
-	pariDispari = "pari";
-} else {
-	pariDispari = "dispari";
+    if( numeroUtente % 2 == 0 ) {
+        pariDispari = "pari";
+    } else {
+        pariDispari = "dispari";
+        listaNumeriDispari.push(numeroUtente);
+    }  
 }
 
-if(pariDispari = "dispari"){
-    listaNumeriDispari.push(numeroUtente)
-    alert(+listaNumeriDispari)
-}
+alert(listaNumeriDispari)
+
 
