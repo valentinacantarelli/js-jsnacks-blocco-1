5296624e3a2362ac96379982e9eae82755ff77c9
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-25 16:32:04 +0200

    creata struttura base

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+var numeroUtente = [];
+
+for (var i=0; i < numeroUtente.length; i++){
+    numeroUtente = parseInt(prompt("Inserisci un numero:"));
+}
+if 
